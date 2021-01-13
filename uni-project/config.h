f4c3deefbcb3113dae5ef50027db5b431806fdd0
Volodia <vova.bilokopytyi@gmail.com>
2021-01-13 17:12:10 +0200

    Fixes signal reading issues and overflow issues

--- uni-project/config.h
+++ uni-project/config.h
@@ -9,9 +9,14 @@
 #define CONFIG_H_
 
 
-const int kLightSensorThreshold = 8;	// percent
-const int kWaitForPressInterval = 20;	// milliseconds
+const int kLedPin    = 3;
+const int kSensorPin = 0;
+
+const int kLightSensorThreshold = 18;	// percent
+const int kWaitForPressInterval = 50;	// milliseconds
 const int kDotDashBorder		= 250;	// milliseconds of press
+const int kSerialDataRate		= 9600; // baud
+const int kLoopDelay			= 40;	// milliseconds
 
 
 #endif  // CONFIG_H_
--- uni-project/uni-project.ino
+++ uni-project/uni-project.ino
@@ -5,37 +5,29 @@
 #include <string.h>
 #include <math.h>
 
-
-const int kLedPin = 3;
-const int kSensorPin = 0;
-
-char temp_str[1000] = { 0 };  // For output formatting
-
-
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(kLedPin, OUTPUT);
   pinMode(kSensorPin, INPUT);
-  analogReference(INTERNAL4V3);
+  analogReference(0);
   
-  Serial.begin(9600);
-  Serial.println("\n\nSerial output initialized.");
+  Serial.begin(kSerialDataRate);
+  Serial.print("\n\nSerial output initialized. Data rate: ");
+  Serial.println(kSerialDataRate);
 }
 
-int click_start = 0;
+unsigned long click_start  = 0;
+unsigned long last_pressed = 0;
 
-int last_pressed = 0;
 bool clicking = false;
-
-int press_counter = 1;
 
 void loop() {
 #if 1  // For stopping program execution
   int sensor_read = analogRead(kSensorPin);
-  int light_perc = round(sensor_read / 1023.0 * 100);
+  int light_perc  = round(sensor_read / 1023.0 * 100);
   bool key_pressed = light_perc <= kLightSensorThreshold;
   
-  if (((millis() - last_pressed) > kWaitForPressInterval) && key_pressed) {
+  if (key_pressed) {
     digitalWrite(kLedPin, HIGH);
 
     last_pressed = millis();
@@ -44,7 +36,7 @@ void loop() {
       clicking = true;
       click_start = millis();
     }
-  } 
+  }
 
   if (!key_pressed) {
     digitalWrite(kLedPin, LOW);
@@ -54,12 +46,12 @@ void loop() {
     clicking = false;
 
     int click_dur = millis() - click_start;
-
     if (click_dur < kDotDashBorder)
       Serial.print(".");
     else
       Serial.print("-");
   }
 
+  delay(kLoopDelay);
 #endif
 }
